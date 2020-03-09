2000fcdac7d80cb84d995a4e2e48626d59cc3b4d
sakhan8-radar <57330603+sakhan8-radar@users.noreply.github.com>
2020-03-09 11:45:10 -0400

    Update and rename Read_&_Write_ydata.m to Read_ydata.m

--- Read_ydata.m
+++ Read_ydata.m
@@ -4,25 +4,6 @@ receivers = 1; % text write and read doesn't work for receivers = 2
 frames = 5; % how many frames do you want recorded?
 current_line = 0;
 j = 1;
-% 
-% ydata = rand(64,2,16,5) + 1i*rand(64,2,16,5);
-% ydata(64,1,16,2)
-
-% Made up ydata matrix
-% R = rand(4,2,2,2)
-% I = 1i*rand(4,2,2,2)
-% ydata_write = R + I
-
-% % % Upload ydata matrix to text file (can only upload receiver 1 data)
-% % (This algorithm doesn't work with two complex number columns)
-% fileID = fopen('ydataRx1.txt','w');
-% while j <= frames
-% for i = 1:chirps
-%     fprintf(fileID,'%f %f\n',[real(ydata(:,1,i,j)),imag(ydata(:,1,i,j))].'); 
-% end
-% j = j+1;
-% end
-% fclose(fileID); % outside of while loop
 
 % % Read ydata matrix from text file (can only upload data 1 receiver at a time)
 % Read Rx1 & Rx2 data in ydataRx1.txt and ydataRx2.txt
