044468b0c193736392c832e9315e7d6a03d3086d
sakhan8-radar <57330603+sakhan8-radar@users.noreply.github.com>
2020-03-09 11:42:42 -0400

    Update Read_&_Write_ydata.m

--- Read_&_Write_ydata.m
+++ Read_&_Write_ydata.m
@@ -1,28 +1,44 @@
-frames = 16; % chirps per frame
+chirps = 16; % chirps per frame
 samples = 64; % samples per chirp
 receivers = 1; % text write and read doesn't work for receivers = 2
+frames = 5; % how many frames do you want recorded?
 current_line = 0;
+j = 1;
+% 
+% ydata = rand(64,2,16,5) + 1i*rand(64,2,16,5);
+% ydata(64,1,16,2)
 
 % Made up ydata matrix
-% R = rand(4,2,2);
-% I = 1i*rand(4,2,2);
-% ydata_write = R + I;
+% R = rand(4,2,2,2)
+% I = 1i*rand(4,2,2,2)
+% ydata_write = R + I
 
 % % % Upload ydata matrix to text file (can only upload receiver 1 data)
 % % (This algorithm doesn't work with two complex number columns)
-% fileID = fopen('ydata.txt','w');
-% for i = 1:frames
-%     fprintf(fileID,'%f %f\n',[real(ydata_write(:,1,i)),imag(ydata_write(:,1,i))].');
+% fileID = fopen('ydataRx1.txt','w');
+% while j <= frames
+% for i = 1:chirps
+%     fprintf(fileID,'%f %f\n',[real(ydata(:,1,i,j)),imag(ydata(:,1,i,j))].'); 
 % end
-% fclose(fileID);
+% j = j+1;
+% end
+% fclose(fileID); % outside of while loop
 
-% % Read ydata matrix from text file (can only upload receiver 1)
-ydata_read = zeros(samples,receivers,frames)
-input = load('ydata.txt')
-for h = 1:(frames)
+% % Read ydata matrix from text file (can only upload data 1 receiver at a time)
+% Read Rx1 & Rx2 data in ydataRx1.txt and ydataRx2.txt
+ydata_readRx1 = zeros(samples,receivers,chirps,frames);
+ydata_readRx2 = zeros(samples,receivers,chirps,frames);
+input1 = load('ydataRx1.txt');
+input2 = load('ydataRx2.txt');
+while j <= frames
+for h = 1:(chirps)
     for g = 1:samples
-        ydata_read(g,1,h) = input(g+current_line,1) + 1i*input(g+current_line,2);
+        ydata_readRx1(g,1,h,j) = input1(g+current_line,1) + 1i*input1(g+current_line,2);
+        ydata_readRx2(g,1,h,j) = input2(g+current_line,1) + 1i*input2(g+current_line,2);
     end
     current_line = current_line+g;
 end
-disp(ydata_read) % print ydata to command line (only contains information for receiver 1
+j = j+1;
+end
+disp(ydata_readRx1) % Receiver 1 ydata stored in ydata_readRx1 array
+disp(ydata_readRx2) % Receiver 2 ydata stored in ydata_readRx1 array
